b2436058811d57f6411b7ff11b9dbb3bc778b15a
zra <takouaefraim@gmail.com>
2023-10-06 17:05:01 +0200

    use key word open

--- projetKotlin/src/main/kotlin/example/myapp/Aquarium.kt
+++ projetKotlin/src/main/kotlin/example/myapp/Aquarium.kt
@@ -2,7 +2,7 @@ package example.myapp
 
 class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
 
-
+/*
     init {
         println("aquarium initializing")
     }
@@ -19,20 +19,31 @@ class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40)
 
 
     //let define a int value o test getter
-
-
-    var volume: Int
+*/
+    open val shape = "rectangle"
+    open var water : Double = 0.0
+        get() = volume * 0.9
+    open var volume: Int
         get() = width * height * length / 1000
         private set(value) {
             height = (value * 1000) / (width * length)
         }
     fun printSize() {
+            /*println("Width: $width cm " +
+                    "Length: $length cm " +
+                    "Height: $height cm "
+            )
+            // 1 liter = 1000 cm^3
+            println("Volume: $volume liters")
+
+             */
+        println(shape)
         println("Width: $width cm " +
                 "Length: $length cm " +
-                "Height: $height cm "
-        )
-        // 1 liter = 1000 cm^3
-        println("Volume: $volume liters")
+                "Height: $height cm ")
+        // 1 l = 1000 cm^3
+        println("Volume: $volume liters Water: $water liters (${water / volume * 100.0}% full)")
+
     }
 
 
--- projetKotlin/src/main/kotlin/example/myapp/Main.kt
+++ projetKotlin/src/main/kotlin/example/myapp/Main.kt
@@ -29,10 +29,11 @@ val myAquarium = Aquarium()
 // aquarium6.printSize()
 // println("Volume: ${aquarium6.width * aquarium6.length * aquarium6.height / 1000} liters")
 //
-//
-    val aquarium6 = Aquarium(numberOfFish = 29)
-    aquarium6.printSize()
-    aquarium6.volume = 70
+// val aquarium6 = Aquarium(numberOfFish = 29)
+// aquarium6.printSize()
+// aquarium6.volume = 70
+// aquarium6.printSize()
+    val aquarium6 = Aquarium(length = 25, width = 25, height = 40)
     aquarium6.printSize()
 }
 //
